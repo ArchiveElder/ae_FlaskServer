3ebf3fe6a4fe41460d1d68c9f8bb1d3be79563b7
Vector1331 <tpgus4796@gmail.com>
2022-07-23 22:17:49 +0900

    [Feat] 이미지 분류 기능 step1 DB에서 음식리스트 불러오기

--- model/FoodClassDao.py
+++ model/FoodClassDao.py
@@ -1,32 +1,46 @@
+import pymysql
 from sqlalchemy import text
 from keras.models import load_model
 from keras import utils
 import numpy as np
 import os
-food_list = ['apple_pie', 'baby_back_ribs', 'baklava', 'beef_carpaccio', 'beef_tartare', 'beet_salad', 'beignets', 'bibimbap', 'bread_pudding', 'breakfast_burrito', 'bruschetta', 'caesar_salad', 'cannoli', 'caprese_salad', 'carrot_cake', 'ceviche', 'cheesecake', 'cheese_plate', 'chicken_curry', 'chicken_quesadilla', 'chicken_wings', 'chocolate_cake', 'chocolate_mousse', 'churros', 'clam_chowder', 'club_sandwich', 'crab_cakes', 'creme_brulee', 'croque_madame', 'cup_cakes', 'deviled_eggs', 'donuts', 'dumplings', 'edamame', 'eggs_benedict', 'escargots', 'falafel', 'filet_mignon', 'fish_and_chips', 'foie_gras', 'french_fries', 'french_onion_soup', 'french_toast', 'fried_calamari', 'fried_rice', 'frozen_yogurt', 'garlic_bread', 'gnocchi', 'greek_salad', 'grilled_cheese_sandwich', 'grilled_salmon', 'guacamole', 'gyoza', 'hamburger', 'hot_and_sour_soup', 'hot_dog', 'huevos_rancheros', 'hummus', 'ice_cream', 'lasagna', 'lobster_bisque', 'lobster_roll_sandwich', 'macaroni_and_cheese', 'macarons', 'miso_soup', 'mussels', 'nachos', 'omelette', 'onion_rings', 'oysters', 'pad_thai', 'paella', 'pancakes', 'panna_cotta', 'peking_duck', 'pho', 'pizza', 'pork_chop', 'poutine', 'prime_rib', 'pulled_pork_sandwich', 'ramen', 'ravioli', 'red_velvet_cake', 'risotto', 'samosa', 'sashimi', 'scallops', 'seaweed_salad', 'shrimp_and_grits', 'spaghetti_bolognese', 'spaghetti_carbonara', 'spring_rolls', 'steak', 'strawberry_shortcake', 'sushi', 'tacos', 'takoyaki', 'tiramisu', 'tuna_tartare', 'waffles']
 
+my_model = load_model('./trainedModel/model_trained.h5')
+food_list = []
 class FoodClassDao:
 
     def __init__(self, database):
         self.db = database
 
+
     def predictFood(self, filename):
-        print('filename is : ' + filename)
-        my_model = load_model('./trainedModel/model_trained.h5')
+        fl = self.db.execute("SELECT * FROM food101")
+        print("food list (from RDS) is :")
+        while (True):
+            row = self.db.fetchone()  # row에 커서(테이블 셀렉트)를 한줄 입력하고 다음줄로 넘어감
+            if row == None:
+                break
+            food_list.append(row[1])
+
+        print(food_list)
+
+        print('uploaded filename (from USER) is : ' + filename)
+
         img = utils.load_img('./static/'+filename, grayscale=False, color_mode='rgb', target_size=(299,299))
         img = utils.img_to_array(img)
         img = np.expand_dims(img, axis=0)
         img /= 255.
 
         pred = my_model.predict(img)
-        index = np.argmax(pred)  # Returns the indices of the maximum values along an axis, In case of multiple occurrences of the maximum values, the indices corresponding to the first occurrence are returned.
+        index = np.argmax(pred)
+
         food_list.sort()
         pred_value = food_list[index]
         print(pred_value)
 
         if os.path.exists('./static/'+filename):
             os.remove('./static/'+filename)
-
+            print('파일이 로컬에서 제거되었습니다.')
         return 'classification result is  : ' +pred_value
 
     def foodNutrient(self,food_type):
